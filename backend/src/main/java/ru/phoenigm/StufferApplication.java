d21b018dd1de55710af83e070a8760803bac9b1d
Azat Mukhametzyanov <phoenigm@ya.ru>
2019-04-05 14:30:51 +0300

    refactor: entry point rename

--- backend/src/main/java/ru/phoenigm/StufferApplication.java
+++ backend/src/main/java/ru/phoenigm/StufferApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootVuejsApplication {
+public class StufferApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootVuejsApplication.class, args);
+		SpringApplication.run(StufferApplication.class, args);
 	}
 }
