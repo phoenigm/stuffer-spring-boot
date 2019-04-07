1d9a2a5fe0ab118847e5288ee0d2e9010dcba2e4
Azat Mukhametzyanov <phoenigm@ya.ru>
2019-04-07 21:34:05 +0300

    refactor

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,29 +2,12 @@ import Vue from 'vue'
 import App from '@/App'
 import router from '@/router/router'
 import store from '@/store/store'
-import {AXIOS} from '@/api/http-common'
 import Vuetify from 'vuetify'
 import '@babel/polyfill'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont'
 
-AXIOS.defaults.withCredentials = true;
 Vue.config.productionTip = false;
-Vue.prototype.$axios = AXIOS;
-
-AXIOS.interceptors.response.use(response => {
-        return Promise.resolve(response)
-    },
-    error => {
-        if (error.response.status === 401) {
-            console.log('Unauthorized, logging out ...');
-            store.dispatch('userSignOut')
-            router.replace('signIn')
-            return Promise.reject(error)
-        } else {
-            return Promise.reject(error.response);
-        }
-    });
 
 Vue.use(Vuetify);
 
