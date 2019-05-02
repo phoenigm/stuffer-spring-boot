261dca5dff17f1258e7af429cd4eff789dc42322
Azat Mukhametzyanov <phoenigm@ya.ru>
2019-05-03 00:38:50 +0300

    refactor: remove test code

--- frontend/src/pages/Settings.vue
+++ frontend/src/pages/Settings.vue
@@ -12,64 +12,6 @@
     export default {
         name: "Settings",
         components: {NavigationBar},
-
-        data: () => ({
-            departureLocalityData: [],
-            departureRegionData: [],
-            isLoading: false,
-            model: null,
-            search: null,
-            isLoading1: false,
-            model1: null,
-            search1: null
-        }),
-
-        computed: {
-            localities() {
-                return this.departureLocalityData.map(e => e.name)
-            },
-            regions() {
-                return this.departureRegionData.map(e => e.name)
-            }
-        },
-
-        watch: {
-            search(val) {
-
-                // Items have already been requested
-                if (this.isLoading) return
-
-                this.isLoading = true
-
-                // Lazily load input items
-                AXIOS.get(`/api/trip/localities?q=${val}&region=${this.model1}`)
-                    .then(res => {
-                        this.localityData = res.data;
-                    })
-                    .catch(err => {
-                        console.log(err)
-                    })
-                    .finally(() => (this.isLoading = false))
-            },
-
-            search1(val) {
-
-                // Items have already been requested
-                if (this.isLoading1) return
-
-                this.isLoading1 = true
-
-                // Lazily load input items
-                AXIOS.get('/api/trip/regions?q=' + val)
-                    .then(res => {
-                        this.regionData = res.data;
-                    })
-                    .catch(err => {
-                        console.log(err)
-                    })
-                    .finally(() => (this.isLoading1 = false))
-            },
-        },
     }
 </script>
 
